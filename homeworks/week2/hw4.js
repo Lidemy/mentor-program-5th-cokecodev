6695327261a45c725b96e7413112362224db7fab
cokecode <you@example.com>
2021-05-04 22:56:01 +0800

    hw4-ver1

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,42 @@
 function printFactor(n) {
-  
-}
+  for(i=1; i<=n; i++){
+      if (i===1){
+          console.log(1)
+      } else if(n%i===0){
+          console.log(i)
+      } else {
+          continue
+      }
 
-printFactor(10);
+      }
+    }
+
+printFactor(7)
+
+
+
+
+/*
+## hw4：印出因數
+先幫大家複習一下數學，給定一個數字 n，因數就是所有小於等於 n 又可以被 n 整除的數，所以最明顯的例子就是 1 跟 n，這兩個數一定是 n 的因數。現在請寫出一個函式來「印出」所有的因數
+
+```
+printFactor(10)
+
+正確輸出：
+1
+2
+5
+10
+```
+
+```
+printFactor(7)
+
+正確輸出：
+1
+7
+```
+
+
+*/
