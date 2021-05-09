4ca1e2e36afaba0d092681b3a57c257043f1bbd6
cokecode <you@example.com>
2021-05-10 00:54:30 +0800

    hw2-Ver1

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,30 @@
+var result=''
 function capitalize(str) {
-  
-}
+    
+  if(str[0]>='a' && str[0]<='z'){
+      result+= String.fromCharCode(str.charCodeAt(0)-32)
+  } else if(str[0]<='a' && str[0]<='z'){
+    result+=str[0]
+  }for(var i=1; i<str.length; i++){
+      result+=str[i]
+  }
+  return result
+    }
 
-console.log(capitalize('hello'));
+
+console.log(capitalize(',hellow'));
+
+
+
+/*
+給定一字串，把第一個字轉成大寫之後「回傳」，若第一個字不是英文字母則忽略。
+
+capitalize('nick')
+正確回傳值：Nick
+
+capitalize('Nick')
+正確回傳值：Nick
+
+capitalize(',hello')
+正確回傳值：,hello
+ */
