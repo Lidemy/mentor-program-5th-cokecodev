a2464703f9f3a4e5e65e276f0ef85ea4b4d97ca9
cokecode <you@example.com>
2021-05-10 00:53:23 +0800

    hw1-Ver1

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,13 @@
-function printStars(n) {
 
+
+function printStars(n) {
+for (let i=1; i<=n; i++){
+    console.log('*')
+}
 }
 
-printStars(5)
+printStars(30)
+
+/*
+給定 n（1<=n<=30），依照規律「印出」正確圖形
+*/
